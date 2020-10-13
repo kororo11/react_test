9529010f23245da66af48c8646d2bf88ee8dd0c2
kororo11 <kororo11@naver.com>
2020-10-13 19:03:06 +0900

    getMovies 함수 생성
    json을 받는 getMovies 함수를 따로 생성하고  componentDidMoun 됐을 때 호출하도록 했다.
    getMovies 함수에서는 json을 다 받으면 isLoading을 false 로 바뀌도록 했다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
     state = {
         isLoading: true,
+        movies: [],
+    };
+
+    getMovies = async () => {
+        const movies = await axios.get(
+            'https://yts-proxy.now.sh/list_movies.json',
+        );
+        this.setState({ movies, isLoading: false });
     };
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({ isLoading: false });
-        }, 6000);
+        this.getMovies({ isLoading: false });
     }
 
     render() {
