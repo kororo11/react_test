f6f4755ef3cbd4fe3de44786b29efac5120406f5
kororo11 <kororo11@naver.com>
2020-10-14 07:29:42 +0900

    장르를 나타내주는 genres 추가

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({ id, title, year, summary, poster }) {
+function Movie({ id, title, year, summary, poster, genres }) {
     return (
         <div className="movie">
             <img src={poster} alt={title} title={title} />
@@ -10,6 +10,13 @@ function Movie({ id, title, year, summary, poster }) {
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
                 <p className="movie__summary">{summary}</p>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
@@ -21,6 +28,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
