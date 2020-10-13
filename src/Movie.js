babf5c4ba0cbda4710800d6975672877a9008f60
kororo11 <kororo11@naver.com>
2020-10-14 07:33:46 +0900

    summary 를 140자까지만 보여주게 slice 했다.

--- src/Movie.js
+++ src/Movie.js
@@ -9,7 +9,7 @@ function Movie({ id, title, year, summary, poster, genres }) {
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 140)}</p>
                 <ul className="genres">
                     {genres.map((genre, index) => (
                         <li key={index} className="genres__genre">
