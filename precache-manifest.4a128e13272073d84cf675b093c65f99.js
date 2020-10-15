8b88b49491fb9b6e8845341ea1eea0d7a9153458
kororo11 <kororo11@naver.com>
2020-10-15 15:56:21 +0900

    Updates

---
+++ precache-manifest.4a128e13272073d84cf675b093c65f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8f1cbecc6ad41f2f57f9f8f038ad17",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "c41d091daee3c3611e95",
+    "url": "/react_test/static/css/main.89232099.chunk.css"
+  },
+  {
+    "revision": "cc6c732a0434c8049779",
+    "url": "/react_test/static/js/2.3c6ede6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_test/static/js/2.3c6ede6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41d091daee3c3611e95",
+    "url": "/react_test/static/js/main.ed2565ad.chunk.js"
+  },
+  {
+    "revision": "0acfb237109b04b55a98",
+    "url": "/react_test/static/js/runtime-main.8b15b1c3.js"
+  }
+]);
